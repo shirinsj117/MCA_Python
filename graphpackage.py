236e4fedecb46ab9ac69b146ff547128148c9c84
Shirin Saji Jacob <shirinshine205@gmail.com>
2024-12-03 21:08:45 -0800

    Add files via upload

---
+++ graphpackage.py
@@ -0,0 +1,17 @@
+from graphics import circle,rectangle #importing circle and rectangle function from graphics package
+r=int(input("Enter the radius of circle:"))
+circle.areac(r)
+circle.peric(r)
+l=int(input("\nEnter the length of rectangle:"))
+b=int(input("Enter the breadth of rectangle:"))
+rectangle.arear(l,b)
+rectangle.perir(l,b)
+from graphics.dgraphics import cuboid,sphere
+l1=int(input("\nEnter the length of cuboid:"))
+b1=int(input("Enter the breadth of cuboid:"))
+h1=int(input("Enter the height of cuboid:"))
+cuboid.areacub(l1,b1,h1)
+cuboid.pericub(l1,b1,h1)
+r1=int(input("\nEnter the radius of sphere:"))
+sphere.areas(r1)
+sphere.peris(r1)
